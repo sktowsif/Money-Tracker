0f0dc316c7efd5a80200022d0dfac63a50f2ff6d
saivarma <saivarma442@gmail.com>
2014-07-23 16:43:16 +0530

    query

--- Money-Tracker/Money-Tracker/Default.aspx.cs
+++ Money-Tracker/Money-Tracker/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Money_Tracker.EntityClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,9 @@ namespace Money_Tracker
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            DateTime i= DateTime.Today;
+            Expenses obj = new Expenses();
+            obj.GetAll(i);
 
         }
     }
---
+++ Money-Tracker/Money-Tracker/EntityClasses/Expenses.cs
@@ -0,0 +1,56 @@
+﻿using SqlConnectorLib;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Money_Tracker.EntityClasses
+{
+    public class Expenses
+    {
+        public int User_id{get;set;}
+        public decimal Expence{get;set;}
+
+        public DateTime Date { get; set; }
+
+        public int Category_Id{get;set;}
+
+        public  void GetAll(DateTime date)
+        {
+            //string[] strColValuesWorkers = Properties.Settings.Default.TierCols.Split('|');
+            //object[] objArrColValuesWorkers = { this.TierId, this.Tier, this.TierMultiplier};
+            string[] strColValuesTier = {};
+            object[] objArrColValuesTier = { };
+            SqlConLib objSqlConLib = new SqlConLib(Properties.Settings.Default.ConnectionString);
+            DataTable dt=objSqlConLib.SelectQuery(@"select top 1 date from Expense", strColValuesTier, objArrColValuesTier);
+            string str1=dt.Rows[0]["date"].ToString();
+            DateTime ad = (DateTime)dt.Rows[0]["date"];
+            DateTime dtdate=ad.AddDays(7);
+            while (dtdate <= date)
+            {
+                
+                object[] objArrColValuesTier1 = { ad,dtdate};
+                string[] strColValuesTier1 = { "ad", "dtdate"};
+                SqlConLib objSqlConLib1 = new SqlConLib(Properties.Settings.Default.ConnectionString);
+                DataTable dtTable = objSqlConLib1.SelectQuery(@"Select sum(Expence) from expense where date between @ad and @dtdate" , strColValuesTier1, objArrColValuesTier1);
+                SqlConLib objSqlConLib2 = new SqlConLib(Properties.Settings.Default.ConnectionString);
+                DataTable dtTable1 = objSqlConLib2.SelectQuery(@"Select sum(Income) from Income where date between @ad and @dtdate", strColValuesTier1, objArrColValuesTier1);
+                ad = dtdate;
+                dtdate = ad.AddDays(7);
+                
+            }
+            //DataTable dtTable = objSqlConLib.SelectQuery(@"Select sum(Expence) from expense where date between"+, strColValuesTier, objArrColValuesTier);
+            //List<Expenses> lstTiers = new List<Expenses>();
+            //for (int i = 0; i < dtTable.Rows.Count; i++)
+            //{
+            //    Expenses objTiers = new Expenses();
+            //    objTiers.User_id =int.Parse( dtTable.Rows[i]["User_id"].ToString());
+            //    objTiers.Expence =decimal.Parse(dtTable.Rows[i]["Expence"].ToString());
+            //    objTiers.Date = dtTable.Rows[i]["Date"].ToString();
+            //    objTiers.Category_Id = int.Parse(dtTable.Rows[i]["Category_id"].ToString());
+            //    lstTiers.Add(objTiers);
+            //}
+        }
+    }
+}
